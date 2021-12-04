9216159f9fde7efc0bc5e95691eaef132dd974d7
Jordan Patton <jordanpatton@gmail.com>
2021-12-03 19:40:20 -0800

    content bump

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Title',
+  title: 'Title 1',
   tagline: 'Tagline goes here.',
   url: 'https://jordanpatton.com',
   baseUrl: '/docusaurus-sandbox/',
