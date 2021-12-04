0edcd8475fdd6d64f2d4932dc8f24e21164a8b34
Jordan Patton <jordanpatton@gmail.com>
2021-12-03 18:40:46 -0800

    url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://example.com',
+  url: 'https://jordanpatton.github.io',
   baseUrl: '/docusaurus-sandbox/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
